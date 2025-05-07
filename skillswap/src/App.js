3d2214d0cb61fa0861b9f8167fc1304c8fc1e251
Imesha Madhumali <imeshamadhumali31@gmail.com>
2025-05-07 09:39:07 +0530

    Create the Login.js and update App.js file with auth.js file

--- skillswap/src/App.js
+++ skillswap/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState}  from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/Home';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import UserTypeSelectionPage from './pages/UserType';
 
 function App() {
+ // const [loggedIn, setLoggedIn] = useState(false);
+  //const [showRegister, setShowRegister] = useState(false);
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/usertype" element={<UserTypeSelectionPage />} />
+        {/* Add more routes like /services, /tasks, /community etc. */}
+      </Routes>
     </div>
+  </Router>
   );
 }
 
