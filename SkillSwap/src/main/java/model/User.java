596f157b0ba4f485cdb33ecbd92baf49df6eb791
Imesha Madhumali <imeshamadhumali31@gmail.com>
2025-05-25 18:07:39 +0530

    Create user model

---
+++ SkillSwap/src/main/java/model/User.java
@@ -0,0 +1,85 @@
+package model;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name="users")
+public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable=false)
+	private String name;
+	
+	@Column(nullable=false,unique=true)
+	private String email;
+	
+	@Column(nullable=false)
+	private String password;
+	
+	@Column(name="created_at")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createdAt;
+	
+	
+
+	public User(String name, String email, String password, Date createdAt) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.createdAt = createdAt;
+	}
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	
+}
+
+
